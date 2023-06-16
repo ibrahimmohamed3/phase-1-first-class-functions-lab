72ce75de4a49f9b873bdccc821258d9a8c274b20
ibrahimmohamed3 <ibra39167271@gmail.com>
2023-06-16 17:19:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2);
+  }
+  
+  
+  function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+  }
+  
+  
